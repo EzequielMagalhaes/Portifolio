f0d759bfaf08dd16caef26c151f2a56e6495af2c
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-01-17 12:16:53 -0300

    feat: Adding cursor pointer to 'Navbar' component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <nav class="flex justify-between items-center font-roboto">
-    <h1 class="text-4xl text-gray-300 font-bold tracking-[0.10em]">&lt;Portifolio/&gt;</h1>
+    <h1 class="text-4xl text-gray-300 font-bold tracking-[0.10em] cursor-pointer">&lt;Portifolio/&gt;</h1>
     <ul class="flex space-x-10">
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li class="cursor-pointer">Home</li>
+        <li class="cursor-pointer">About</li>
+        <li class="cursor-pointer">Projects</li>
+        <li class="cursor-pointer">Contact</li>
     </ul>
   </nav>
 </template>
