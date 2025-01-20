7dee19147502d4d1823deb6a0785d5d0a73327fa
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-01-20 12:33:51 -0300

    style: Created glow effect in terminal buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'red-glow': '0 0 10px 2px rgba(239, 68, 68, 0.8)', // Vermelho
+        'yellow-glow': '0 0 10px 2px rgba(234, 179, 8, 0.8)', // Amarelo
+        'green-glow': '0 0 10px 2px rgba(34, 197, 94, 0.8)', // Verde
+      },
+    },
   },
   plugins: [],
 }
