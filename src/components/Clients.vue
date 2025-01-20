89be3e4b17da1c58ef236c7e86b7d59a7cc62e29
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-01-19 23:42:08 -0300

    feat: Creating the client feedback page

---
+++ src/components/Clients.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col gap-5">
+        <h1 class="text-3xl font-bold text-gray-300">Hear from my clients</h1>
+        <div class="grid grid-cols-2 gap-5">
+            <!-- CLIENT 1 -->
+            <div class="p-5 rounded-lg bg-gray-900">
+                <p class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien.</p>
+                <div class="">
+                    <div class="">
+
+                    </div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
