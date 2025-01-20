9131bf941ecfbeca1d1b0612b5872b362e32c350
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-01-20 15:19:16 -0300

    feat: Add smooth scroll-to-top button with hover effect

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,30 @@
   <div class="p-5 px-20 flex flex-col gap-5">
     <Navbar/>
     <hr class="border-t-2 border-gray-300"/>
-    <Header/>
-    <About/>
-    <Project/>
-    <Clients/>
-    <Experience/>
-    <Contact/>
+    <section id="home">
+      <Header/>
+    </section>
+    <section id="about">
+      <About/>
+    </section>
+    <section id="projects">
+      <Project/>
+    </section>
+    <section id="clients">
+      <Clients/>
+    </section>
+    <section id="experience">
+      <Experience/>
+    </section>
+    <section id="contact">
+      <Contact/>
+    </section>
     <Footer/>
+    <transition name="fade">
+      <button v-show="showScrollButton" @click="scrollToTop" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" class="fixed bottom-5 right-5 bg-neutral-300 p-3 rounded-full hover:bg-black">
+        <img :src="scrollButtonImage" class="h-6 -rotate-45" alt="teste">
+      </button>
+    </transition>
   </div>
 </template>
 
@@ -23,5 +40,50 @@ import Contact from './components/Contact.vue'
 import Footer from './components/Footer.vue'
 export default {
   components: { Navbar, Header, About, Project, Clients, Experience, Contact, Footer },
+  data() {
+    return {
+      showScrollButton: false,
+      scrollButtonImage: '/src/assets/black-site-arrow.png'
+    };
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    },
+    handleScroll() {
+      const projectsSection = document.getElementById('projects');
+      if (projectsSection) {
+        const rect = projectsSection.getBoundingClientRect();
+        this.showScrollButton = rect.top <= window.innerHeight;
+      }
+    },
+    handleMouseEnter() {
+      this.scrollButtonImage = '/src/assets/white-site-arrow.png';
+    },
+    handleMouseLeave() {
+      this.scrollButtonImage = '/src/assets/black-site-arrow.png';
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.7s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+button:hover {
+  background-color: black;
+}
+button:hover img {
+  content: url('/src/assets/white-site-arrow.png');
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,16 +2,23 @@
   <nav class="flex justify-between items-center font-roboto">
     <h1 class="text-4xl text-neutral-300 font-bold tracking-[0.10em] cursor-pointer">&lt;Portifolio/&gt;</h1>
     <ul class="flex space-x-10">
-        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2">Home</li>
-        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2">About</li>
-        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2">Projects</li>
-        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2">Contact</li>
+        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('home')">Home</li>
+        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('about')">About</li>
+        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('projects')">Projects</li>
+        <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('contact')">Contact</li>
     </ul>
   </nav>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    scrollToSection(sectionId) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }
 }
 </script>
