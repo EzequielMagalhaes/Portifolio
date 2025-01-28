056aa8468e2102f93cdba569a40a49599c152dd3
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-01-28 14:30:36 -0300

    feat: Adding navbar language tooltip

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,4 +1,3 @@
-// filepath: /c:/Users/zekes/OneDrive/Área de Trabalho/PC - ZEKIEL/Projetos/Portifolio/frontend/src/components/Navbar.vue
 <template>
   <nav class="flex justify-between items-center font-roboto">
     <h1 class="text-4xl text-neutral-300 font-bold tracking-[0.10em] cursor-pointer">{{ $t('title') }}</h1>
@@ -7,16 +6,25 @@
       <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('projects')">{{ $t('navbar.projects') }}</li>
       <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('experience')">{{ $t('navbar.experience') }}</li>
       <li class="cursor-pointer hover:bg-neutral-300 hover:text-black rounded-full p-2" @click="scrollToSection('contact')">{{ $t('navbar.contact') }}</li>
-      <li class="relative cursor-pointer" @mouseenter="showBlackTranslate" @mouseleave="hideBlackTranslate" @click="toggleLanguage">
+      <li class="relative cursor-pointer" @click="toggleLanguage">
         <img src="/src/assets/translate.svg" class="h-8" id="translate" alt="translate">
         <img src="/src/assets/black-translate.svg" class="h-8 absolute top-0 left-0 opacity-0 bg-gray-300 rounded-lg" id="black-translate" alt="black-translate">
+        <div v-if="showTooltip" ref="tooltip" class="tooltip">{{ currentLanguage }}</div>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import { computePosition, offset, flip, shift } from '@floating-ui/dom';
+
 export default {
+  data() {
+    return {
+      showTooltip: false,
+      currentLanguage: this.$i18n.locale === 'en' ? 'English' : 'Português'
+    };
+  },
   methods: {
     scrollToSection(sectionId) {
       const section = document.getElementById(sectionId);
@@ -24,16 +32,59 @@ export default {
         section.scrollIntoView({ behavior: 'smooth' });
       }
     },
-    showBlackTranslate() {
-      document.getElementById('black-translate').style.opacity = '1';
-    },
-    hideBlackTranslate() {
-      document.getElementById('black-translate').style.opacity = '0';
-    },
     toggleLanguage() {
       const currentLocale = this.$i18n.locale;
       this.$i18n.locale = currentLocale === 'en' ? 'pt' : 'en';
+      this.currentLanguage = this.$i18n.locale === 'en' ? 'English' : 'Português';
+
+      // Exibe o tooltip e atualiza a posição
+      this.showTooltip = false; // Garante que o estado seja reiniciado
+      this.$nextTick(() => {
+        this.showTooltip = true;
+        this.updateTooltipPosition();
+      });
+
+      // Oculta o tooltip após 2 segundos
+      setTimeout(() => {
+        this.showTooltip = false;
+      }, 2000);
+    },
+    updateTooltipPosition() {
+      const button = document.getElementById('translate');
+      const tooltip = this.$refs.tooltip;
+
+      if (button && tooltip) {
+        computePosition(button, tooltip, {
+          placement: 'bottom',
+          middleware: [offset(10), flip(), shift()],
+        }).then(({ x, y }) => {
+          Object.assign(tooltip.style, {
+            left: `${x}px`,
+            top: `${y}px`,
+          });
+        });
+      }
+    }
+  },
+  watch: {
+    showTooltip(newVal) {
+      if (newVal) {
+        this.updateTooltipPosition();
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+.tooltip {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 5px 10px;
+  border-radius: 3px;
+  font-size: 12px;
+  white-space: nowrap;
+  z-index: 1000;
+}
+</style>
