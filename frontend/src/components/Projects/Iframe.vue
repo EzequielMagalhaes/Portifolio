fe4d634c71055169ddc756b646917b69f5705efb
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-02-10 10:27:00 -0300

    fix: Fixed the iframe size

--- frontend/src/components/Projects/Iframe.vue
+++ frontend/src/components/Projects/Iframe.vue
@@ -3,7 +3,7 @@
         v-if="currentProject"
         :key="currentProject.site"
         :src="currentProject.site" 
-        class="col-span-4 h-[667px] w-[505px] scale-[0.9] md:h-[600px] md:w-[839px] md:scale-[1] sm:h-[154%] sm:w-[154%] sm:scale-[0.65] rounded-lg bg-center bg-cover bg-no-repeat object-scale-down relative bg-white iframe"
+        class="col-span-4 h-[667px] w-[505px] md:h-[923px] md:w-[850px] sm:h-[154%] sm:w-[154%] sm:scale-[0.65] rounded-lg bg-center bg-cover bg-no-repeat object-scale-down relative bg-white iframe"
         style="transform-origin: 0 0;">
     </iframe>
 </template>
